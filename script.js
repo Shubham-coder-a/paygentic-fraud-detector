1cbb161b8e30885f45a00f5c38be49f2f1038501
Shubham Nagpure <shubhamnagpure989@gmail.com>
2026-05-06 23:28:40 +0530

    Added fraud detection system with risk analysis and transaction history

--- script.js
+++ script.js
@@ -14,7 +14,40 @@ function checkFraud() {
 
     let score = document.getElementById("score");
 
+    let transactionId = document.getElementById("transactionId");
+
+    let historyBody = document.getElementById("historyBody");
+
+    let reason = document.getElementById("reason");
+
     let progressBar = document.getElementById("progress-bar");
+
+    let randomId = Math.floor(Math.random() * 100000);
+
+    transactionId.innerHTML = "Transaction ID: TXN" + randomId;
+
+
+    // INVALID AMOUNT
+    if (amount <= 0) {
+
+        result.innerHTML = "❌ Invalid Amount";
+        result.style.color = "red";
+
+        riskLevel.innerHTML = "🚫 Invalid Input";
+        riskLevel.style.color = "red";
+
+        score.innerHTML = "Fraud Score: 0%";
+        score.style.color = "red";
+
+        transactionId.innerHTML = "Transaction ID: INVALID";
+
+        reason.innerHTML = "Reason: Amount cannot be 0 or negative";
+        reason.style.color = "red";
+
+        progressBar.style.width = "0%";
+
+        return;
+    }
 
 
     // HIGH RISK
@@ -22,6 +55,8 @@ function checkFraud() {
         amount > 50000 ||
         location.toLowerCase() === "unknown" ||
         (time >= "00:00" && time <= "04:00")
+
+        
     ) {
 
         result.innerHTML = "⚠️ Suspicious Transaction";
@@ -30,13 +65,24 @@ function checkFraud() {
         riskLevel.innerHTML = "🔴 High Risk";
         riskLevel.style.color = "red";
 
+        reason.innerHTML = "Reason: High Amount / Late Night Transaction";
+        reason.style.color = "red";
+
         score.innerHTML = "Fraud Score: 90%";
         score.style.color = "red";
 
         progressBar.style.width = "90%";
         progressBar.style.backgroundColor = "red";
+        
+        historyBody.innerHTML += `
+<tr>
+    <td>${amount}</td>
+    <td>${location}</td>
+    <td style="color:red;">High Risk</td>
+</tr>
+`;
     }
-
+    
 
     // MEDIUM RISK
     else if (amount > 10000) {
@@ -47,13 +93,22 @@ function checkFraud() {
         riskLevel.innerHTML = "🟡 Medium Risk";
         riskLevel.style.color = "orange";
 
+        reason.innerHTML = "Reason: Medium Transaction Amount";
+        reason.style.color = "orange";
+
         score.innerHTML = "Fraud Score: 55%";
         score.style.color = "orange";
 
         progressBar.style.width = "55%";
         progressBar.style.backgroundColor = "orange";
+        historyBody.innerHTML += `
+<tr>
+    <td>${amount}</td>
+    <td>${location}</td>
+    <td style="color:orange;">Medium Risk</td>
+</tr>
+`;
     }
-
 
     // LOW RISK
     else {
@@ -64,10 +119,20 @@ function checkFraud() {
         riskLevel.innerHTML = "🟢 Low Risk";
         riskLevel.style.color = "green";
 
+        reason.innerHTML = "Reason: Normal Safe Activity";
+        reason.style.color = "green";
+
         score.innerHTML = "Fraud Score: 10%";
         score.style.color = "green";
 
         progressBar.style.width = "10%";
         progressBar.style.backgroundColor = "green";
+        historyBody.innerHTML += `
+<tr>
+    <td>${amount}</td>
+    <td>${location}</td>
+    <td style="color:green;">Low Risk</td>
+</tr>
+`;
     }
 }
