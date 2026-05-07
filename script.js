ad59ffe85484d056f9188d8ee77bd627999f493e
Shubham Nagpure <shubhamnagpure989@gmail.com>
2026-05-07 23:58:19 +0530

    Added AI scanning effect, dark mode, report download and transaction history

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function checkFraud() {
 
+    let scannerText = document.getElementById("scannerText");
+
     let amount = Number(document.getElementById("amount").value);
 
     let location = document.getElementById("location").value;
@@ -22,134 +24,281 @@ function checkFraud() {
 
     let progressBar = document.getElementById("progress-bar");
 
-    let randomId = Math.floor(Math.random() * 100000);
 
-    transactionId.innerHTML = "Transaction ID: TXN" + randomId;
+    // RESET OLD DATA
+    result.innerHTML = "";
 
-    let blacklistedLocations = [
-        "pakistan",
-        "russia",
-        "darkweb",
-        "unknown"
-    ];
+    riskLevel.innerHTML = "";
 
+    score.innerHTML = "";
 
-    // INVALID AMOUNT
-    if (amount <= 0 || isNaN(amount)) {
+    reason.innerHTML = "";
 
-        result.innerHTML = "❌ Invalid Amount";
-        result.style.color = "red";
-
-        riskLevel.innerHTML = "🚫 Invalid Input";
-        riskLevel.style.color = "red";
-
-        score.innerHTML = "Fraud Score: 0%";
-        score.style.color = "red";
-
-        transactionId.innerHTML = "Transaction ID: INVALID";
-
-        reason.innerHTML = "Reason: Amount cannot be 0 or negative";
-        reason.style.color = "red";
-
-        progressBar.style.width = "0%";
-        progressBar.style.backgroundColor = "gray";
-
-        return;
-    }
+    transactionId.innerHTML = "";
 
 
-    // HIGH RISK
-    if (
-        amount > 50000 ||
-        blacklistedLocations.includes(location.toLowerCase()) ||
-        (time >= "00:00" && time <= "04:00")
-    ) {
+    // SCANNING EFFECT
+    scannerText.innerHTML = "🔍 AI Scanning Transaction...";
 
-        result.innerHTML = "⚠️ Suspicious Transaction";
-        result.style.color = "red";
-
-        riskLevel.innerHTML = "🔴 High Risk";
-        riskLevel.style.color = "red";
-
-        reason.innerHTML =
-            "Reason: High Amount / Blacklisted Location / Late Night Transaction";
-        reason.style.color = "red";
-
-        score.innerHTML = "Fraud Score: 90%";
-        score.style.color = "red";
-
-        progressBar.style.width = "90%";
-        progressBar.style.backgroundColor = "red";
-
-        historyBody.innerHTML += `
-        <tr>
-            <td>${amount}</td>
-            <td>${location}</td>
-            <td style="color:red;">High Risk</td>
-        </tr>
-        `;
-    }
+    scannerText.style.color = "#00bfff";
 
 
-    // MEDIUM RISK
-    else if (amount > 10000) {
+    setTimeout(() => {
 
-        result.innerHTML = "⚠️ Monitor Transaction";
-        result.style.color = "orange";
+        let randomId = Math.floor(Math.random() * 100000);
 
-        riskLevel.innerHTML = "🟡 Medium Risk";
-        riskLevel.style.color = "orange";
-
-        reason.innerHTML = "Reason: Medium Transaction Amount";
-        reason.style.color = "orange";
-
-        score.innerHTML = "Fraud Score: 55%";
-        score.style.color = "orange";
-
-        progressBar.style.width = "55%";
-        progressBar.style.backgroundColor = "orange";
-
-        historyBody.innerHTML += `
-        <tr>
-            <td>${amount}</td>
-            <td>${location}</td>
-            <td style="color:orange;">Medium Risk</td>
-        </tr>
-        `;
-    }
+        transactionId.innerHTML = "Transaction ID: TXN" + randomId;
 
 
-    // LOW RISK
-    else {
+        let blacklistedLocations = [
 
-        result.innerHTML = "✅ Safe Transaction";
-        result.style.color = "green";
+            "pakistan",
 
-        riskLevel.innerHTML = "🟢 Low Risk";
-        riskLevel.style.color = "green";
+            "russia",
 
-        reason.innerHTML = "Reason: Normal Safe Activity";
-        reason.style.color = "green";
+            "darkweb",
 
-        score.innerHTML = "Fraud Score: 10%";
-        score.style.color = "green";
+            "unknown"
 
-        progressBar.style.width = "10%";
-        progressBar.style.backgroundColor = "green";
-
-        historyBody.innerHTML += `
-        <tr>
-            <td>${amount}</td>
-            <td>${location}</td>
-            <td style="color:green;">Low Risk</td>
-        </tr>
-        `;
-    }
+        ];
 
 
-    // CLEAR INPUTS
-    document.getElementById("amount").value = "";
-    document.getElementById("location").value = "";
-    document.getElementById("paymentMethod").value = "";
-    document.getElementById("time").value = "";
+        // INVALID AMOUNT
+        if (amount <= 0 || isNaN(amount)) {
+
+            result.innerHTML = "❌ Invalid Amount";
+
+            result.style.color = "red";
+
+
+            riskLevel.innerHTML = "🚫 Invalid Input";
+
+            riskLevel.style.color = "red";
+
+
+            score.innerHTML = "Fraud Score: 0%";
+
+            score.style.color = "red";
+
+
+            transactionId.innerHTML = "Transaction ID: INVALID";
+
+
+            reason.innerHTML = "Reason: Amount cannot be 0 or negative";
+
+            reason.style.color = "red";
+
+
+            progressBar.style.width = "0%";
+
+            progressBar.style.backgroundColor = "gray";
+        }
+
+
+        // HIGH RISK
+        else if (
+
+            amount > 50000 ||
+
+            blacklistedLocations.includes(location.toLowerCase()) ||
+
+            (time >= "00:00" && time <= "04:00")
+
+        ) {
+
+            result.innerHTML = "⚠️ Suspicious Transaction";
+
+            result.style.color = "red";
+
+
+            riskLevel.innerHTML = "🔴 High Risk";
+
+            riskLevel.style.color = "red";
+
+
+            reason.innerHTML =
+                "Reason: High Amount / Blacklisted Location / Late Night Transaction";
+
+            reason.style.color = "red";
+
+
+            score.innerHTML = "Fraud Score: 90%";
+
+            score.style.color = "red";
+
+
+            progressBar.style.width = "90%";
+
+            progressBar.style.backgroundColor = "red";
+
+
+            historyBody.innerHTML += `
+
+            <tr>
+
+                <td>${amount}</td>
+
+                <td>${location}</td>
+
+                <td style="color:red;">High Risk</td>
+
+            </tr>
+
+            `;
+        }
+
+
+        // MEDIUM RISK
+        else if (amount > 10000) {
+
+            result.innerHTML = "⚠️ Monitor Transaction";
+
+            result.style.color = "orange";
+
+
+            riskLevel.innerHTML = "🟡 Medium Risk";
+
+            riskLevel.style.color = "orange";
+
+
+            reason.innerHTML = "Reason: Medium Transaction Amount";
+
+            reason.style.color = "orange";
+
+
+            score.innerHTML = "Fraud Score: 55%";
+
+            score.style.color = "orange";
+
+
+            progressBar.style.width = "55%";
+
+            progressBar.style.backgroundColor = "orange";
+
+
+            historyBody.innerHTML += `
+
+            <tr>
+
+                <td>${amount}</td>
+
+                <td>${location}</td>
+
+                <td style="color:orange;">Medium Risk</td>
+
+            </tr>
+
+            `;
+        }
+
+
+        // LOW RISK
+        else {
+
+            result.innerHTML = "✅ Safe Transaction";
+
+            result.style.color = "green";
+
+
+            riskLevel.innerHTML = "🟢 Low Risk";
+
+            riskLevel.style.color = "green";
+
+
+            reason.innerHTML = "Reason: Normal Safe Activity";
+
+            reason.style.color = "green";
+
+
+            score.innerHTML = "Fraud Score: 10%";
+
+            score.style.color = "green";
+
+
+            progressBar.style.width = "10%";
+
+            progressBar.style.backgroundColor = "green";
+
+
+            historyBody.innerHTML += `
+
+            <tr>
+
+                <td>${amount}</td>
+
+                <td>${location}</td>
+
+                <td style="color:green;">Low Risk</td>
+
+            </tr>
+
+            `;
+        }
+
+
+        // SCAN COMPLETE
+        scannerText.innerHTML = "✅ Scan Complete";
+
+        scannerText.style.color = "lime";
+
+
+        // CLEAR INPUTS
+        document.getElementById("amount").value = "";
+
+        document.getElementById("location").value = "";
+
+        document.getElementById("paymentMethod").value = "";
+
+        document.getElementById("time").value = "";
+
+    }, 2000);
+}
+
+
+
+function downloadReport() {
+
+    let reportData = `
+
+Paygentic Fraud Detector Report
+
+--------------------------------
+
+${document.getElementById("result").innerText}
+
+${document.getElementById("riskLevel").innerText}
+
+${document.getElementById("score").innerText}
+
+${document.getElementById("transactionId").innerText}
+
+${document.getElementById("reason").innerText}
+
+--------------------------------
+
+Generated by Paygentic Fraud Detector
+
+`;
+
+
+    let blob = new Blob([reportData], { type: "text/plain" });
+
+
+    let link = document.createElement("a");
+
+
+    link.href = URL.createObjectURL(blob);
+
+
+    link.download = "fraud-report.txt";
+
+
+    link.click();
+}
+
+
+
+function toggleMode() {
+
+    document.body.classList.toggle("dark-mode");
 }
